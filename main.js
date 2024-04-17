cf5a9f6c620ff39a26a665255c440ca3b5227d87
Meseverri <meseverrimarquez5@gmail.com>
2024-04-17 08:47:40 +0200

    main to init change + vite svg deleted

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 import './style.css'
 import { header } from './src/views/header/header'
+import { init } from './src/views/init/init'    
 
 const app$$=document.querySelector("#app")
 header(app$$)
+init(app$$)
