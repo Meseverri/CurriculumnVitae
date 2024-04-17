38ceac914d60c140537cd5ac018c4b21b07d0ac5
Meseverri <meseverrimarquez5@gmail.com>
2024-04-17 08:19:21 +0200

    initial

---
+++ main.js
@@ -0,0 +1,5 @@
+import './style.css'
+import { header } from './src/views/header/header'
+
+const app$$=document.querySelector("#app")
+header(app$$)
---
+++ src/views/header/header.js
@@ -0,0 +1,29 @@
+import "./header.css"
+
+export const header=(app)=>{
+    const header$$=document.createElement("header");
+    const ret = `
+    <h1>Peter Parker</h1>
+    <nav>
+      <ul>
+        <li>
+          <a href="#aboutme">About me</a>
+        </li>
+        <li>
+          <a href="#education">Education</a>
+        </li>
+        <li>
+          <a href="#experience">Experience</a>
+        </li>
+        <li>
+          <a href="#projects">Projects</a>
+        </li>
+      </ul>
+    </nav>
+    `
+    header$$.innerHTML=ret;
+    app.append(header$$);
+
+
+
+}
