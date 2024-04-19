7c5272052e63fb38a61a8ede774ee88a57340433
Meseverri <meseverrimarquez5@gmail.com>
2024-04-19 09:29:47 +0200

    header Refactoring

--- src/views/header/header.js
+++ src/views/header/header.js
@@ -1,30 +1,23 @@
-import "./header.css"
+import "./header.css";
 import data from "/src/data/data.js";
+import { list } from "/src/components/list/list";
+export const header = (app) => {
+  const header$$ = document.createElement("header");
+  const h1$$=document.createElement( "h1" );
+  h1$$.textContent=data.name
+  header$$.appendChild(h1$$)
+  const nav$$=document.createElement("nav");
+  list(
+    ["About me", "Education", "Experience", "Projects"],
+    "header-nav-ul",
+    nav$$,
+    "a"
+  );
+  const aList=nav$$.querySelectorAll("a");
+  aList.forEach(element => {
+  element.setAttribute("href",`#${element.innerText.replace(/\s+/g, '').toLowerCase()}`);
+  });
 
-export const header=(app)=>{
-    const header$$=document.createElement("header");
-    const ret = `
-    <h1>${data.name}</h1>
-    <nav>
-      <ul>
-        <li>
-          <a href="#aboutme">About me</a>
-        </li>
-        <li>
-          <a href="#education">Education</a>
-        </li>
-        <li>
-          <a href="#experience">Experience</a>
-        </li>
-        <li>
-          <a href="#projects">Projects</a>
-        </li>
-      </ul>
-    </nav>
-    `
-    header$$.innerHTML=ret;
-    app.append(header$$);
-
-
-
-}
+  header$$.appendChild(nav$$);
+  app.append(header$$);
+};
