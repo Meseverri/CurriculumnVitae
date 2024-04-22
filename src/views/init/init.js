662ccc9b0cc21d0502620b806b4f12f7595b5891
Meseverri <meseverrimarquez5@gmail.com>
2024-04-22 10:44:35 +0200

    education

--- src/views/init/init.js
+++ src/views/init/init.js
@@ -2,12 +2,13 @@ import "./init.css";
 import {aboutMe} from "/src/initViews/aboutMe/aboutMe"
 import {projects} from "/src/initViews/projects/projects"
 import {experience} from "/src/initViews/experience/experience"
-
+import {education} from "/src/initViews/education/education"
 
 export const init = (data,app) => {
   const main$$ = document.createElement("main");
   aboutMe(data,main$$);
   projects(data,main$$);
   experience(data,main$$)
+  education(data,main$$);
   app.appendChild(main$$);
 };
