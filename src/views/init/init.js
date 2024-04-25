f65f6b248ff89ae733d47ae9bdefb66d8d4f4fee
Meseverri <meseverrimarquez5@gmail.com>
2024-04-25 10:02:35 +0200

    change section button first integration

--- src/views/init/init.js
+++ src/views/init/init.js
@@ -8,7 +8,32 @@ export const init = (data,app) => {
   const main$$ = document.createElement("main");
   aboutMe(data,main$$);
   projects(data,main$$);
+ //button change
+  const changeDiv=document.createElement("div");
+  const buttonChange=document.createElement("button");
+  buttonChange.setAttribute("class","change")
+  buttonChange.textContent="Show education";
+  changeDiv.appendChild(buttonChange);
+  main$$.appendChild(changeDiv);
+  
+  
   experience(data,main$$)
-  education(data,main$$);
   app.appendChild(main$$);
+  
+  let experienceBool=true;
+  buttonChange.addEventListener("click",()=>{
+    if (experienceBool){
+      const changeSection=document.querySelector(".experience");
+      changeSection.remove();
+      experienceBool=false;
+      buttonChange.textContent="Show experience";
+      education(data,main$$);
+    }else{
+      const changeSection=document.querySelector(".education");
+      changeSection.remove();
+      experienceBool=true;
+      buttonChange.textContent="Show education";
+      experience(data,main$$);
+    }
+  })
 };
