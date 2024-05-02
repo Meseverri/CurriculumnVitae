d7b7884bc91c1ccfddecb21448f6ce6c97c27fef
Meseverri <meseverrimarquez5@gmail.com>
2024-05-03 00:10:31 +0200

    remove content buton function

--- src/events/events.js
+++ src/events/events.js
@@ -1,20 +1,22 @@
 import { experience } from "/src/initViews/experience/experience";
 import { education } from "/src/initViews/education/education";
 
+const removeContent=(modal,textContent,data)=>{
+  modal.removeChild(modal.childNodes[0]);
+  button.textContent = textContent;
+  education(data, modal);
+}
+
 export const contentChangeEvent = (button,data) => {
   const changeSection$$ = document.querySelector("#content");
   let experienceBool = true;
   button.addEventListener("click", () => {
     if (experienceBool) {
-      changeSection$$.removeChild(changeSection$$.childNodes[0]);
       experienceBool = false;
-      button.textContent = "Show experience";
-      education(data, changeSection$$);
+      removeContent(changeSection$$, "Show experience",data);
     } else {
-      changeSection$$.removeChild(changeSection$$.childNodes[0]);
       experienceBool = true;
-      button.textContent = "Show education";
-      experience(data, changeSection$$);
+      removeContent(changeSection$$, "Show education",data);
     }
   });
 };
