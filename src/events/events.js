ce0104b50eb75e18dec783e95a1e4bb482f003f2
Meseverri <meseverrimarquez5@gmail.com>
2024-05-02 16:11:53 +0200

    education center content

--- src/events/events.js
+++ src/events/events.js
@@ -4,9 +4,7 @@ import { education } from "/src/initViews/education/education";
 export const contentChangeEvent = (button,data) => {
   const changeSection$$ = document.querySelector("#content");
   let experienceBool = true;
-  console.log(button);
   button.addEventListener("click", () => {
-    console.log("aqui tas");
     if (experienceBool) {
       changeSection$$.removeChild(changeSection$$.childNodes[0]);
       experienceBool = false;
