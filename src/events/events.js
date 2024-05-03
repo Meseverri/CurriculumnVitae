50bc3058d666b1e466860a9145f4473ff65b7759
Meseverri <meseverrimarquez5@gmail.com>
2024-05-03 08:15:55 +0200

    boton bugs fixed

--- src/events/events.js
+++ src/events/events.js
@@ -1,22 +1,23 @@
 import { experience } from "/src/initViews/experience/experience";
 import { education } from "/src/initViews/education/education";
 
-const removeContent=(modal,textContent,data)=>{
+const removeContent=(modal,textContent,data,expBool,button)=>{
   modal.removeChild(modal.childNodes[0]);
   button.textContent = textContent;
-  education(data, modal);
+  expBool? experience(data,modal):education(data, modal);
+  
 }
 
 export const contentChangeEvent = (button,data) => {
   const changeSection$$ = document.querySelector("#content");
-  let experienceBool = true;
+  let experienceBool = false;
   button.addEventListener("click", () => {
     if (experienceBool) {
+      removeContent(changeSection$$, "Show experience",data,experienceBool,button);
       experienceBool = false;
-      removeContent(changeSection$$, "Show experience",data);
     } else {
+      removeContent(changeSection$$, "Show education",data,experienceBool,button);
       experienceBool = true;
-      removeContent(changeSection$$, "Show education",data);
     }
   });
 };
